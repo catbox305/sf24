36b84dc67c637808d8326931e48ad5a43e17e57a
catbox305 <109403545+catbox305@users.noreply.github.com>
2024-01-20 11:24:00 -0800

    .

--- gui2.py
+++
@@ -1,5 +0,0 @@
-import random
-
-strings = ["".join(random.choices("qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890".split(""),k=20)) for i in range(100000)]
-data = [{"string":i} for i in strings]
-
